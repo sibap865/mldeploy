381480f44d3ea8bcf68032be61d23ddaeb1fcd22
sibaprasad <sibap865@gmail.com>
2023-04-25 22:18:21 +0530

    EDA and MODEL training in google collab

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-import logger
+from src.logger import logging
 def error_message_details(error,err_details:sys):
     _,_,exc_tb=err_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -16,11 +15,3 @@ class CustomException(Exception):
         self.error_message=error_message_details(error_message,err_details=error_details)
     def __str__(self):
         return self.error_message
-    
-
-if __name__ =="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("devide by zero error")
-        raise CustomException(e,sys)
